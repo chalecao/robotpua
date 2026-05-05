13510c1295831a92f3fecc2d1e2a6c7f7ed30fd0
chale <chale@chaledeMacBook-Air.local>
2026-05-05 21:58:22 +0800

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Target, Zap, Square, Crosshair, Trophy } from 'lucide-react';
+import { Target, Zap, Square, Trophy } from 'lucide-react';
 import Navbar from '../components/Navbar';
 
 const games = [
